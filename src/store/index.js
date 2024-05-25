f3f3eac1a974a4431dd6aa84429982891e45ece9
Prafull Karanje <prafullkaranje@gmail.com>
2024-05-25 14:55:55 +0530

    with eslnt err

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,11 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: () => ({
+    destinations: [
+      "brazil", 
+      "panama", "hawaii", "jamaica"]
+  }),
   getters: {},
   mutations: {},
   actions: {},
