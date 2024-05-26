1fd7ff11fffd59d708385ab92f1b16a0604de4e9
Prafull Karanje <prafullkaranje@gmail.com>
2024-05-26 11:42:49 +0530

    GO back button

---
+++ src/components/HistoryBack.vue
@@ -0,0 +1,22 @@
+<template>
+  <div @click="goBack" class="goBack">
+    <slot name="icon"> ← </slot>
+    <slot> Back </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      console.log('going back');
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+    }
+  }
+}
+</script>
+<style scoped>
+.goBack {
+  cursor: pointer;
+}
+</style>
--- src/views/TheNavBar.vue
+++ src/views/TheNavBar.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="">
+    <HistoryBack>
+      <template v-slot:icon> ◄ </template>
+      Go Back
+    </HistoryBack>
     <div v-for="(destination) in destinations" :key="destination.name">
-        <router-link :to="{name: 'DestinationDetails' , params : {...destination} }" >{{ destination.name }}</router-link>
+      <router-link :to="{ name: 'DestinationDetails', params: { ...destination } }">{{ destination.name }}</router-link>
     </div>
     <hr>
   </div>
 </template>
 
 <script>
+import HistoryBack from '@/components/HistoryBack.vue';
+
 export default {
   name: "TheNavBar",
   data() {
@@ -15,6 +21,10 @@ export default {
       destinations: this.$store.getters.destinations
     }
   },
+  components: {
+    HistoryBack
+  },
+
   computed: {
     // // if data inside this.$store.getters.destinations is changing ferquently 
     // // then use this as this part is reactive
