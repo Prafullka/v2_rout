0f6de80fc72103d793460ce9f5a4ef9cfa856028
prafullka <prafullkaranje@yahoo.com>
2024-05-26 18:05:14 +0530

    as

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   // publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
-  publicPath: process.env.NODE_ENV === "production" ? "/v2_rout/" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/v2_rout/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/v2_rout/dist/" : "/",
 });
